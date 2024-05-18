acee8622bcb4a4cd36a727db33593fafa318b183
Diego Ignacio Ortega Martínez <diegortegaimi@gmail.com>
2024-05-18 16:44:12 -0600

    Refactor peak coefficient generation

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -38,11 +38,72 @@ SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor()
 void SimpleEQAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll (juce::Colours::black);
+    
+    auto bounds = getLocalBounds();
+    
+    auto responseArea = bounds.removeFromTop(bounds.getHeight() * 0.33);
+    
+    auto w = responseArea.getWidth();
+    
+    auto& lowcut = monochain.get<ChainPositions::LowCut>();
+    auto& peak = monochain.get<ChainPositions::Peak>();
+    auto& highcut = monochain.get<ChainPositions::HighCut>();
+    
+    auto sampleRate = audioProcessor.getSampleRate();
+    
+    std::vector<double> mags;
+    
+    mags.resize(w);
+    
+    for( int i = 0; i< w; ++i ){
+        double mag = 1.f;
+        auto freq = juce::mapToLog10(double(i) / double(w), 20.0, 20000.0);
+        
+        if(!monochain.isBypassed<ChainPositions::Peak>())
+            mag *= peak.coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        
+        if(!lowcut.isBypassed<0>())
+            mag *= lowcut.get<0>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if(!lowcut.isBypassed<1>())
+            mag *= lowcut.get<1>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if(!lowcut.isBypassed<2>())
+            mag *= lowcut.get<2>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if(!lowcut.isBypassed<3>())
+            mag *= lowcut.get<3>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        
+        if(!highcut.isBypassed<0>())
+            mag *= highcut.get<0>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if(!highcut.isBypassed<1>())
+            mag *= highcut.get<1>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if(!highcut.isBypassed<2>())
+            mag *= highcut.get<2>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        if(!highcut.isBypassed<3>())
+            mag *= highcut.get<3>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+        
+        mags[i] = juce::Decibels::gainToDecibels(mag);
+    }
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    juce::Path responseCurve;
+    
+    const double outputMin = responseArea.getBottom();
+    const double outputMax = responseArea.getY();
+    auto map = [outputMin, outputMax](double input)
+    {
+        return juce::jmap(input, -24.0, 24.0, outputMin, outputMax);
+    };
+    
+    responseCurve.startNewSubPath(responseArea.getX(), map(mags.front()));
+    
+    for( size_t i = 1; i < mags.size(); ++i){
+        responseCurve.lineTo(responseArea.getX() + i, map(mags[i]));
+    }
+    
+    g.setColour (juce::Colours::orange);
+    g.drawRoundedRectangle(responseArea.toFloat(), 4.f, 1.f);
+    
+    g.setColour(juce::Colours::white);
+    g.strokePath(responseCurve, juce::PathStrokeType(2.f));
 }
 
 void SimpleEQAudioProcessorEditor::resized()
@@ -62,6 +123,24 @@ void SimpleEQAudioProcessorEditor::resized()
     peakFreqSlider.setBounds(bounds.removeFromTop(bounds.getHeight() * 0.33));
     peakGainSlider.setBounds(bounds.removeFromTop(bounds.getHeight() * 0.5));
     peakQualitySlider.setBounds(bounds);
+}
+
+void SimpleEQAudioProcessorEditor::timerCallback()
+{
+    if(parameterChanged.compareAndSetBool(false, true)){
+        // update the monochain
+        // signal a repaint
+    }
+}
+
+void SimpleEQAudioProcessorEditor::parameterValueChanged(int parameterIndex, float newValue)
+{
+    parameterChanged.set(true);
+}
+
+void SimpleEQAudioProcessorEditor::parameterGestureChanged(int parameterIndex, bool gestureIsStarting)
+{
+    parameterChanged.set(true);
 }
 
 std::vector<juce::Component*> SimpleEQAudioProcessorEditor::getComps()
