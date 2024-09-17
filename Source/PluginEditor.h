10de2d3a57a2ab6ebedceca08563ebb4f2bde9a2
Diego Ignacio Ortega Martínez <diegortegaimi@gmail.com>
2024-09-17 00:07:28 -0600

    placed slider circles in final position

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -22,7 +22,7 @@ struct LookAndFeel : juce::LookAndFeel_V4
                           float     rotaryStartAngle,
                           float     rotaryEndAngle,
                           juce::Slider & slider
-                          ) override {}
+                          ) override;
 };
 
 struct RotarySliderWithLabels : juce::Slider
@@ -41,7 +41,7 @@ struct RotarySliderWithLabels : juce::Slider
         setLookAndFeel(nullptr);
     }
     
-    void paint(juce::Graphics& g) override {}
+    void paint(juce::Graphics& g) override;
     juce::Rectangle<int> getSliderBounds() const;
     int getTextHeight() const { return 14; }
     juce::String getDisplayString() const;
